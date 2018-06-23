19cea684a4f874015317f0fd8c24c1fcc170c443
Gustavo Santos <gfdsantos@inf.ufpel.edu.br>
2018-06-22 23:15:03 -0300

    added comentaries

--- src/zilez.js
+++ src/zilez.js
@@ -5,16 +5,30 @@ const ppf = require('./pretty-path-formatter');
 
 let watcher, db, rootPath;
 
+/**
+ * Generate a signature of a file
+ * @param {string} filepath 
+ */
 function signature(filepath) {
 	const file = fs.readFileSync(filepath, { encoding: 'utf-8' });
 	return sha256(file).toString();
 }
 
+/**
+ * Compare if a file has the signature
+ * @param {string} sign signature of a file
+ * @param {string} filepath path to another file
+ */
 function hasSignature(sign, filepath) {
 	const file = fs.readFileSync(filepath, { encoding: 'utf-8' });
 	return sign === sha256(file).toString();
 }
 
+/**
+ * Observe a directory
+ * @param {string} path full path to one directory
+ * @param {function} handler function that handle changes
+ */
 function observe(path, handler) {
 	watcher = chokidar.watch(path, {
 	    ignored: /(^|[\/\\])\../,
@@ -27,6 +41,10 @@ function observe(path, handler) {
 	onChange(handler);
 }
 
+/**
+ * Listen for changes inside directory
+ * @param {Function} listener 
+ */
 function onChange(listener) {
 	watcher.on('add', path => {
 		db.set(ppf(rootPath, path), signature(path));
@@ -75,6 +93,10 @@ function onChange(listener) {
 	});
 }
 
+/**
+ * Get the current state of directory
+ * @returns {object}
+ */
 function snapshot() {
 	const package = {
 		entries: db.entries(),
@@ -85,10 +107,18 @@ function snapshot() {
 	return package;
 }
 
+/**
+ * Return all files and directories inside the root directory
+ * @returns {array}
+ */
 function files() {
 	return db.keys();
 }
 
+/**
+ * Number of files and directories that are tracked
+ * @returns {number}
+ */
 function length() {
 	return db.size || 0;
 }
